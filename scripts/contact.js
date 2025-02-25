c32a2e030e29112d4225702b810ed681cfaff3a0
dido1043 <dudunovski_05@abv.bg>
2025-02-25 17:30:22 +0200

    Added email-sender

---
+++ scripts/contact.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("HEb-pfXU6MhPdXqgS");
+});
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const serviceID = "service_7hunsho"; 
+  const templateID = "template_m266mot"; 
+
+  const formData = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+  };
+
+  emailjs.send(serviceID, templateID, formData)
+      .then(() => {
+          alert("Message sent successfully!");
+      })
+      .catch(() => {
+          alert("Failed to send message. Please try again.");
+      });
+});
